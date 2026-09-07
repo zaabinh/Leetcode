02469db0b2ce62da3d9a01c3dae1c7ff05d2413b
zaabinh <doanbinh2208@gmail.com>
2026-09-07 23:20:28 +0700

    Add new solution for Rotate Array

--- Java/RotateArray/BestSolution.java
+++ Java/RotateArray/BestSolution.java
@@ -1,17 +1,24 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int[] result = new int[n]; 
+    
+        k %= n;
         
-        for(int i = 0; i < n; i++) {
-            result[(i + k) % n] = nums[i];
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while(left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
         }
-        
-        System.arraycopy(result, 0, nums, 0, n);
     }
 }
 
-public class Main {
+public class BestSolution {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         int k = 3;
