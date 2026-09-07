19e59fd3736d66668385da0e9f873dde1a0f8909
zaabinh <doanbinh2208@gmail.com>
2026-09-07 22:47:14 +0700

    Add new empty Python solution for Merge Sorted Arrays

---
+++ Python/MergeSortedArray/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        Merging 2 sorted arrays in-place into a new sorted array
+        
+        :type nums1:List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: Return nothing, modify nums1 in-place instead
+
+        """
+        pass
+        
+
+
+if __name__ == "__main__":
+    s = Solution()
